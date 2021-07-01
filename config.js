1ca5de82dc17805e85b9124b3c114a73a1d512cd
MinnnNgg <86762177+MinnnNgg@users.noreply.github.com>
2021-07-01 13:01:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Sơn à :>!',
-    introDesc: `thật tuyệt vời là em đã sử dụng phần lớn cho chơi game và học hành,giá mà em cụng sử dụng 1 phần nhỏ để quan tâm chị =))`,
+    introTitle: 'Thùy Anhh à :>!',
+    introDesc: `thật tuyệt vời là m đã sử dụng phần lớn cho idol và học hành,giá mà em cụng sử dụng 1 phần nhỏ để quan tâm t =))`,
     btnIntro: 'hjccc',
-    title: 'emm có thương chị hămmm',
-    desc: 'trả lời ik out ra là muốn làm chồng chị á =)) ',
+    title: 'm có thương t hămmm',
+    desc: 'trả lời ik out ra là muốn làm chồng t á =)) ',
     btnYes: 'Cóa vlll <33',
-    btnNo: 'hông, em chỉ xem chị như một ng rất quan trọng thoiii :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
-    btnReply: 'Gửi cho chị <3',
+    btnNo: 'hông, em chỉ xem m như một ng rất quan trọng thoiii :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu t <3',
+    btnReply: 'Gửi cho nanhh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Chị biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay chơi game nhe =))',
+    mess: 'T biết mà 🥰. Yêu m nhiều nhiều 😘😘',
+    messDesc: 'Tối nay mình hẹn hò nheee =))',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //liprofile.php?id=100037592971599
+    messLink: 'http://fb.com' //ngoc.anh.duong.792303
 }
